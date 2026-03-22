041e20f1474d3caccc5d8a8a9e34f2ee84de8957
andeadriano <andeadriano@gmail.com>
2026-03-22 22:45:58 +0000

    instagram adicionado

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -70,7 +70,7 @@ const Contato = () => {
                 Instagram
               </p>
               <p className="text-muted-foreground text-sm">
-                @tridimencionalpilates
+                @tridimensionalpilates
               </p>
             </div>
           </motion.a>
